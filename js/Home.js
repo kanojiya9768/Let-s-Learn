fc42bdb454d06e6ec1b9424c9795941505d0ffd7
Vishal Brijesh kanojiya <90042549+kanojiya9768@users.noreply.github.com>
2022-09-07 23:50:31 +0530

    Update Home.js

---
+++ js/Home.js
@@ -0,0 +1,25 @@
+//get user and make profile and if user is not logged in logout him automatically
+let current_user = JSON.parse(localStorage.getItem('user'));
+
+//set profile
+
+if(!current_user){
+    window.location.assign('index.html');
+}else{
+    document.getElementById('lets_learn').innerText = current_user;
+}
+
+
+//logout functionality
+let logout = () =>{
+    localStorage.removeItem('user');
+    window.location.assign('index.html');
+
+    function noBack() {
+        window.history.forward()
+    }
+    noBack();
+    window.onload = noBack;
+    window.onpageshow = function (evt) { if (evt.persisted) noBack() }
+    window.onunload = function () { void (0) }
+}
